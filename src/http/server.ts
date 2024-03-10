9c5c7150733b1f901aae59626f0d761363fe1e0a
Thiago Santana <thiagosan252@gmail.com>
2024-03-10 16:09:40 -0300

    feat: get managed restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,20 +2,20 @@ import chalk from 'chalk'
 import { Elysia } from 'elysia'
 import { env } from '../env'
 
-import { getRestaurant } from './routes/get-restaurant'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 
 new Elysia()
   .use(registerRestaurant)
-  .use(getRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurant)
   .get('/', () => {
     return 'Hello World'
   })
